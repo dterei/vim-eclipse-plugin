643f3843ab35ba53998f96ccec7f3797b71df2a4
bastl <bastl@e026b6f4-af2c-0410-b418-94eebdd9c5e6>
2006-01-24 09:54:12 +0000

    Included suggestions from Pierre Nakashian - Use a AWT-Panel to avoid flickering - under windows use 'vim' as VIMCMD (has to be on the path)
    git-svn-id: https://vimplugin.svn.sourceforge.net/svnroot/vimplugin/trunk@16 e026b6f4-af2c-0410-b418-94eebdd9c5e6

--- trunk/vimplugin/src/org/vim/vimplugin/editors/VimEditor.java
+++ trunk/vimplugin/src/org/vim/vimplugin/editors/VimEditor.java
@@ -19,12 +19,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import java.awt.BorderLayout;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.awt.SWT_AWT;
 import org.eclipse.swt.widgets.Composite;
+import java.awt.Panel;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.IFileEditorInput;
@@ -56,15 +58,10 @@ public class VimEditor extends EditorPart {
 	
   
   /* constants */
-  private final String VIMCMD = "vim.org";
-
-  // using linux, "vim" flickers enormously, probably a 
-  // problem with doublebuffering in SwingTerminal and SWT_AWT
-  // private final String VIMCMD = "vim";
-
-  // Under Windows it doesnt work at all. All we get is a black screen. Help welcome.
-  //private final String VIMCMD = "c:/Programme/Vim/vim64/vim.exe";
   
+  //To use Windows just replace VIMCMD with 'vim'. 'vim' should be on the path.
+  private final String VIMCMD = "vim.basic";
+
 	/**
 	 * The constructor.
 	 */
@@ -104,25 +101,35 @@ public class VimEditor extends EditorPart {
 		};
 
 
+
     // setup the gui
     st=new SwingTerminal(emulation, new Font("Monospaced", Font.PLAIN, 12));
 		st.setResizeStrategy(SwingTerminal.RESIZE_SCREEN);
 
-		awtComposite = new Composite(parent, SWT.EMBEDDED);
+		awtComposite = new Composite(parent, SWT.NO_BACKGROUND | SWT.EMBEDDED);
 
-    //     the SWT_AWT Bridge is ugly and not free (as in freedom),
-    //     but the guys from JTA are working on a "SWTTerminal"
+    // the SWT_AWT Bridge is ugly and not free (as in freedom),
+    // but the guys from JTA are working on a "SWTTerminal"
     awtFrame = SWT_AWT.new_Frame(awtComposite);
-		
-		awtFrame.addComponentListener(new ComponentAdapter() {
-			public void componentResized(ComponentEvent ce) {
-				int r=st.getVDUBuffer().getRows();
-				int c=st.getVDUBuffer().getColumns();
-				sendCmd(":set lines="+r+" columns="+c+"\n");
-			}
-		});
-		
-		awtFrame.add(st);
+
+    //put the terminal into a AWT-Panel to avoid flickering
+    Panel panel = new Panel(new BorderLayout()) {
+      public void update(java.awt.Graphics g) {
+        /* Do not erase the background */
+        paint(g);
+      }
+    };
+    
+    awtFrame.add(panel);
+    panel.add(st);
+
+    panel.addComponentListener(new ComponentAdapter() {
+      public void componentResized(ComponentEvent ce) {
+        int r = st.getVDUBuffer().getRows();
+        int c = st.getVDUBuffer().getColumns();
+        sendCmd(":set lines=" + r + " columns=" + c + "\n");
+      }
+    });
 
     // get the file to edit
     IFileEditorInput iei = (IFileEditorInput)getEditorInput();
@@ -138,7 +145,7 @@ public class VimEditor extends EditorPart {
 
 	public void sendCmd(String vimCmd) {
 		try {
-			// System.err.println("CMD to vim: "+vimCmd);
+			System.err.println("CMD to vim: "+vimCmd);
 			byte[] b=vimCmd.getBytes();
 			vimOs.write(new byte [] {'', ''});
 			vimOs.write(b);
@@ -168,6 +175,8 @@ public class VimEditor extends EditorPart {
     boolean cancelled = vimjob.cancel();
     //System.err.println("vimjob cancelled: "+cancelled);
   }
+  
+  
   
 
 ////////////////   TODO   /////////////////
