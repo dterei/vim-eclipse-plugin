bcedb5378d028895b7a743aeb854ce0a506d996b
bastl <bastl@e026b6f4-af2c-0410-b418-94eebdd9c5e6>
2008-05-13 15:12:42 +0000

    * maintance release 0.3.4   * deployment via ant (remove deploy.sh/zsh/btm)   * generate dynamic wikipages via ant
    git-svn-id: https://vimplugin.svn.sourceforge.net/svnroot/vimplugin/trunk@228 e026b6f4-af2c-0410-b418-94eebdd9c5e6

--- updatesite/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-USER=bastl
-SITE="$USER@vimplugin.sf.net:/home/groups/v/vi/vimplugin/htdocs/update"
-
-scp "features/org.vimplugin.feature_$1.jar" ${SITE}"/features"
-scp "plugins/org.vimplugin.plugin_$1.jar" ${SITE}"/plugins"
-scp "site.xml" ${SITE}
