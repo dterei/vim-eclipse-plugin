594f04ad93273544c80b26a66bcf91735bc3ad4c
bastl <bastl@e026b6f4-af2c-0410-b418-94eebdd9c5e6>
2008-05-16 16:22:27 +0000

    * allows to send arbitrary commands/keystroke to a vim server via command "vim --remote-send <keysequence>" This is used to let eclipse talk to vim (e.g. open in tab when using VimServer class)
    git-svn-id: https://vimplugin.svn.sourceforge.net/svnroot/vimplugin/trunk@239 e026b6f4-af2c-0410-b418-94eebdd9c5e6

--- plugin/src/org/vimplugin/views/CommandView.java
+++ plugin/src/org/vimplugin/views/CommandView.java
@@ -10,25 +10,72 @@
  */
 package org.vimplugin.views;
 
-import org.eclipse.swt.SWT; 
-import org.eclipse.swt.widgets.*;
-import org.eclipse.ui.part.ViewPart;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.part.ViewPart;
 import org.vimplugin.VimPlugin;
 
+/**
+ * A very simple command launcher for both the netbeans protocol (start command
+ * with nb: then) and "vim --remote-send" commands (start command with "vr:"
+ * then).
+ */
 public class CommandView extends ViewPart {
 
 	private Text input;
 
+	@Override
 	public void createPartControl(Composite parent) {
 		input = new Text(parent, SWT.MULTI);
 		input.addListener(SWT.KeyDown, new Listener() {
 
+			//TODO: cleanup launching of commands inside vim.
 			public void handleEvent(Event e) {
 				System.out.println("Command View: " + e.character);
 				if (e.character == 0x0D) {
-					VimPlugin.getDefault().getVimserver(0).getVc().plain(
-							input.getText());
+
+					String line1 = input.getText().substring(3);
+					if (input.getText().startsWith("vr:")) {
+						String[] args = { "vim", "--servername", "GVIM",
+								"--remote-send", line1 };
+						try {
+							System.out.printf("running %s :", Arrays
+									.toString(args));
+							Process process = new ProcessBuilder(args).start();
+
+							InputStream is = process.getInputStream();
+							InputStreamReader isr = new InputStreamReader(is);
+							BufferedReader br = new BufferedReader(isr);
+							String line;
+
+							while ((line = br.readLine()) != null) {
+								System.out.println(line);
+							}
+							process.waitFor();
+
+							System.out
+									.println("result: " + process.exitValue());
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						} catch (InterruptedException e2) {
+							// TODO Auto-generated catch block
+							e2.printStackTrace();
+						}
+					} else if (input.getText().startsWith("nb:")) {
+						VimPlugin.getDefault().getVimserver(0).getVc().plain(
+								line1);
+					}
+
 					input.setText("");
 				}
 
@@ -36,6 +83,7 @@ public class CommandView extends ViewPart {
 		});
 	}
 
+	@Override
 	public void setFocus() {
 		input.setFocus();
 	}
